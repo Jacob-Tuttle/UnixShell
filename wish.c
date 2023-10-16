c6faeec9c1c165d85f5956638dab438e0c0bf18d
Jacob-Tuttle <147278166+Jacob-Tuttle@users.noreply.github.com>
2023-10-16 15:01:37 -0700

    read in commands from line
    Add feature for individual cmd execution from line using strsep(). Also add the ability for ls token execution. Only for interactive mode.

--- wish.c
+++ wish.c
@@ -1,21 +1,68 @@
-int main(int argc, char *argv[]){
-  while(1){
-    char *line = NULL;
-    size_t len = 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-    printf("wish> ");
 
-    // Use getline to read from stdin
-    getline(&line, &len, stdin);
+void interactiveMode(){
 
-    // Remove the trailing newline character
-    line[strcspn(line, "\n")] = '\0';
-    if(strcmp(line,"exit") == 0){
-        // Free dynamically allocated memory
-        free(line);
-        exit(0);
+    while(1){
+        char *line = NULL, *token = NULL;
+        size_t len = 0;
+        char delim = ' ';
 
+
+        printf("wish> ");
+
+        // Use getline to read from stdin
+        getline(&line, &len, stdin);
+        // Remove the trailing newline character
+        line[strcspn(line, "\n")] = '\0';
+
+        //process entered command
+        while((token = strsep(&line, &delim)) != NULL){
+            if(strcmp(token,"exit") == 0){
+                // Free dynamically allocated memory
+                free(line);
+                exit(0);
+            }
+            else if(strcmp(token, "ls") == 0){
+                char *argv[] = {"ls", "-1", NULL};
+                if(access("/bin/ls", X_OK) == 0){
+
+                    execv("/bin/ls", argv);
+                }
+                else if (access( "/usr/bin/ls", X_OK) == 0){
+                    execv("/bin/ls", argv);
+                }
+                else{
+                    printf("Error: command not found\n");
+                }
+            }
+            else{
+                printf("%s\n", token);
+            }
+        }
     }
-  }
+}
+
+void batchRead(){
+}
+
+int main(int argc, char *argv[]){
+
+    //No arguemnt
+    if(argc == 1){
+        interactiveMode();
+    }
+    //batch file
+    else if(argc == 2){
+        batchRead();
+    }
+    //error
+    else{
+        exit(1);
+    }
   return 0;
 }
+
