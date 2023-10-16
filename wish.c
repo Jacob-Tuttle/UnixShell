31eb8a6a366d96a8da46d0811d7d3e4f9e52f2cd
Jacob-Tuttle <147278166+Jacob-Tuttle@users.noreply.github.com>
2023-10-16 16:51:38 -0700

    Fixed LS and added cd
    Fixed ls build in function so it would continue original process instead of exiting after execution(runs on child process instead of main now). Add cd(change directory) function. Work as of now, haven't done extensive testing.

--- wish.c
+++ wish.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void interactiveMode(){
 
@@ -11,7 +12,6 @@ void interactiveMode(){
         size_t len = 0;
         char delim = ' ';
 
-
         printf("wish> ");
 
         // Use getline to read from stdin
@@ -19,24 +19,51 @@ void interactiveMode(){
         // Remove the trailing newline character
         line[strcspn(line, "\n")] = '\0';
 
-        //process entered command
-        while((token = strsep(&line, &delim)) != NULL){
-            if(strcmp(token,"exit") == 0){
+        int argNum = 0;
+        char *temp = line;
+        char **tokens = malloc(sizeof(char *) * len); // Store tokens in an array
+
+        //separates individual tokens into tokens array
+        //will be used for execution below
+        while ((token = strsep(&temp, &delim)) != NULL) {
+            tokens[argNum] = token;
+            argNum += 1;
+        }
+
+        //executes given tokens
+        for(int j = 0; j < argNum; j++){
+            if(strcmp(tokens[j],"exit") == 0){
                 // Free dynamically allocated memory
                 free(line);
                 exit(0);
             }
-            else if(strcmp(token, "ls") == 0){
-                char *argv[] = {"ls", "-1", NULL};
-                if(access("/bin/ls", X_OK) == 0){
-
-                    execv("/bin/ls", argv);
+            else if(strcmp(tokens[j], "ls") == 0){
+                pid_t child_pid = fork();
+                if (child_pid == 0) {
+                    // This code block will be executed by the child process
+                    char *argv[] = {"ls", "-1", NULL};
+                    if (access("/bin/ls", X_OK) == 0) {
+                        execv("/bin/ls", argv);
+                    }
+                    else if (access("/usr/bin/ls", X_OK) == 0) {
+                        execv("/usr/bin/ls", argv);
+                    }
+                    else {
+                        printf("Error: command not found\n");
+                    }
                 }
-                else if (access( "/usr/bin/ls", X_OK) == 0){
-                    execv("/bin/ls", argv);
+                else if (child_pid > 0) {
+                    // This code block will be executed by the parent process
+                    wait(NULL);  // Wait for the child process to finish
+                }
+            }
+            else if(strcmp(tokens[j], "cd") == 0){
+                if(argNum == 2){
+                    chdir(tokens[j+1]);
+                    break;
                 }
                 else{
-                    printf("Error: command not found\n");
+                    printf("Error\n");
                 }
             }
             else{
@@ -44,6 +71,9 @@ void interactiveMode(){
             }
         }
     }
+
+    free(line);
+    free(tokens);
 }
 
 void batchRead(){
@@ -65,4 +95,3 @@ int main(int argc, char *argv[]){
     }
   return 0;
 }
-
