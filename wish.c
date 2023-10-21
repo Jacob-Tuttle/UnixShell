8d6a0d8e6b74b7eb23837b255d406ada811e9a76
Jacob-Tuttle <147278166+Jacob-Tuttle@users.noreply.github.com>
2023-10-21 15:58:32 -0700

    handles variable whitespace either space or tab before after or between cmds and arugments.

--- wish.c
+++ wish.c
@@ -1,37 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void interactiveMode(){
-
+int main() {
     while(1){
-        char *line = NULL, *token = NULL;
-        size_t len = 0;
-        char delim = ' ';
+        char *line = NULL, *token = NULL; //contains the entire line entered || stores individual tokens of line
+        size_t len = 0; //Size of the line
+        char *delim = " \t\n"; //Used for strsep to break line into tokens
+        int Size = 1;
+        int count = 0;
+        char **tokens;
+        tokens = (char **)malloc(Size * sizeof(char*));
 
-        printf("wish> ");
-
-        // Use getline to read from stdin
-        getline(&line, &len, stdin);
-        // Remove the trailing newline character
-        line[strcspn(line, "\n")] = '\0';
-
-        int argNum = 0;
-        char *temp = line;
-        char **tokens = malloc(sizeof(char *) * len); // Store tokens in an array
-
-        //separates individual tokens into tokens array
-        //will be used for execution below
-        while ((token = strsep(&temp, &delim)) != NULL) {
-            tokens[argNum] = token;
-            argNum += 1;
+        //Error allocating
+        if(tokens == NULL){
+            exit(1);
         }
 
-        //executes given tokens
-        for(int j = 0; j < argNum; j++){
+        printf("wish> ");
+        // Use getline to read from stdin
+        //and split line into tokens using strsep
+        getline(&line, &len, stdin);
+        while((token = strsep(&line, delim)) != NULL){
+            //will remove consecutive delimiters
+            if(*token != '\0'){
+                if(count >= Size){
+                    Size +=1;
+                    tokens = (char**)realloc(tokens, Size * sizeof(char*));
+                    tokens[count] = (char*)malloc(strlen(token) + 1);
+                    strcpy(tokens[count],token);
+                    //printf("%d: %s\n",count, tokens[count]);
+                    count+=1;
+
+                }
+                else if(count == 0){
+                    tokens[count] = (char*)malloc(strlen(token) + 1);
+                    strcpy(tokens[count],token);
+                    //printf("%d: %s\n",count, tokens[count]);
+                    count+=1;
+                }
+                else{
+                    exit(1);
+                }
+            }
+        }
+
+        char **arg = (char **)malloc((count + 1) * sizeof(char*)); // Allocate memory for arg
+
+        if (arg == NULL) {
+            exit(1);
+        }
+
+        for (int j = 0; j < count; j++) {
+            arg[j] = (char *)malloc(strlen(tokens[j]) + 1);
+            strcpy(arg[j], tokens[j]);
+        }
+
+        arg[count] = NULL;
+
+        //process
+        for(int j = 0; j < count; j++){
+            token = tokens[j];
             if(strcmp(tokens[j],"exit") == 0){
                 // Free dynamically allocated memory
                 free(line);
@@ -41,14 +73,11 @@ void interactiveMode(){
                 pid_t child_pid = fork();
                 if (child_pid == 0) {
                     // This code block will be executed by the child process
-                    //if the user types ls -la /tmp, your shell should run the program 
-                    //bin/ls with the given arguments -la and /tmp (how does the shell know to run /bin/ls)
-                    char *argv[] = {"ls", "-1", NULL};
                     if (access("/bin/ls", X_OK) == 0) {
-                        execv("/bin/ls", argv);
+                        execv("/bin/ls", arg);
                     }
                     else if (access("/usr/bin/ls", X_OK) == 0) {
-                        execv("/usr/bin/ls", argv);
+                        execv("/usr/bin/ls",arg);
                     }
                     else {
                         printf("Error: command not found\n");
@@ -58,42 +87,23 @@ void interactiveMode(){
                     // This code block will be executed by the parent process
                     wait(NULL);  // Wait for the child process to finish
                 }
+
             }
             else if(strcmp(tokens[j], "cd") == 0){
-                if(argNum == 2){
+                if(count == 2){
                     chdir(tokens[j+1]);
-                    break;
                 }
                 else{
                     printf("Error\n");
                 }
             }
             else{
-                printf("%s\n", token);
+                //printf("no matching cmd\n");
             }
         }
+        free(line);
+        free(tokens);
     }
 
-    free(line);
-    free(tokens);
-}
-
-void batchRead(){
-}
-
-int main(int argc, char *argv[]){
-
-    //No arguemnt
-    if(argc == 1){
-        interactiveMode();
-    }
-    //batch file
-    else if(argc == 2){
-        batchRead();
-    }
-    //error
-    else{
-        exit(1);
-    }
-  return 0;
+    return 0;
 }
