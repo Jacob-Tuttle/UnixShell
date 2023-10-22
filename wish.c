8ff2ab37cb3655527166bd5747bf3d25cd21e9f3
Jacob-Tuttle <147278166+Jacob-Tuttle@users.noreply.github.com>
2023-10-21 18:10:32 -0700

    Update wish.c

--- wish.c
+++ wish.c
@@ -1,17 +1,75 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+bool check_Build_In(char *cmd){
+    char *builtin[3] = {"exit","cd","path"};
+    for(int i = 0; i < 3;i++){
+        if(strcmp(cmd,builtin[i])==0){
+            return true;
+        }
+    }
+    return false;
+}
+
+char* check_access(char **path, char *cmd, int pathSize){
+
+    for(int i = 0; i < pathSize; i++){
+
+        // Calculate the length of the resulting string
+        size_t len = strlen(cmd) + strlen(path[i]);
+
+        // Allocate memory for the new string
+        char *New = (char *)malloc(len + 1);
+
+        if (New != NULL) {
+        // Copy the path into the new string
+            strcpy(New, path[i]);
+
+            // Concatenate the command at the end
+            strcat(New, cmd);
+            if(access(New,X_OK) == 0){
+                return New;
+            }
+        }
+    }
+    return "Empty";
+}
+
+char** update_path(char** currentPaths, int *pathSize, char *add){
+    char** newPaths = (char**)malloc((*pathSize + 1) * sizeof(char*));
+
+    for(int i = 0; i< *pathSize;i++){
+        strcpy(newPaths[i],currentPaths[i]);
+    }
+    *pathSize+=1;
+    newPaths[*pathSize] = (char*)malloc(strlen(add) + 1);
+    strcpy(newPaths[*pathSize], add);
+    free(currentPaths);
+    return newPaths;
+}
+
+char** initilize_path(){
+    char** path = (char**)malloc(1 * sizeof(char*));
+    path[0] = "/bin/";
+    return path;
+}
+
 int main() {
+    char **path;//starting path
+    int pathSize = 1; //nubmer of paths stored
+    path = initilize_path();
+
     while(1){
         char *line = NULL, *token = NULL; //contains the entire line entered || stores individual tokens of line
         size_t len = 0; //Size of the line
         char *delim = " \t\n"; //Used for strsep to break line into tokens
         int Size = 1;
-        int count = 0;
+        int count = 0; //Number of tokens
         char **tokens;
         tokens = (char **)malloc(Size * sizeof(char*));
 
@@ -48,6 +106,8 @@ int main() {
             }
         }
 
+
+        //creates argument array that is null terminated inorder to run execv
         char **arg = (char **)malloc((count + 1) * sizeof(char*)); // Allocate memory for arg
 
         if (arg == NULL) {
@@ -64,23 +124,14 @@ int main() {
         //process
         for(int j = 0; j < count; j++){
             token = tokens[j];
-            if(strcmp(tokens[j],"exit") == 0){
-                // Free dynamically allocated memory
-                free(line);
-                exit(0);
-            }
-            else if(strcmp(tokens[j], "ls") == 0){
+            //if cmd is not build in cmd
+            if(check_Build_In(tokens[j]) == false){
                 pid_t child_pid = fork();
                 if (child_pid == 0) {
                     // This code block will be executed by the child process
-                    if (access("/bin/ls", X_OK) == 0) {
-                        execv("/bin/ls", arg);
-                    }
-                    else if (access("/usr/bin/ls", X_OK) == 0) {
-                        execv("/usr/bin/ls",arg);
-                    }
-                    else {
-                        printf("Error: command not found\n");
+                    char* Loc=check_access(path,tokens[j],pathSize);
+                    if(strcmp(Loc,"Empty") != 0){
+                        execv(Loc,arg);
                     }
                 }
                 else if (child_pid > 0) {
@@ -89,12 +140,26 @@ int main() {
                 }
 
             }
-            else if(strcmp(tokens[j], "cd") == 0){
-                if(count == 2){
-                    chdir(tokens[j+1]);
+            else if(check_Build_In(tokens[j]) == true){
+                if(strcmp(tokens[j],"exit") == 0){
+                    free(line);
+                    exit(0);
                 }
-                else{
-                    printf("Error\n");
+                else if(strcmp(tokens[j],"cd") == 0){
+                    if(count == 2){
+                        chdir(tokens[j+1]);
+                    }
+                    else{
+                        //error
+                    }
+                }
+                else if(strcmp(tokens[j],"path") == 0){
+                    //will update path and pathSize(number of paths stored)
+                    path = update_path(path,&pathSize, tokens[j+1]);
+                    printf("%d", pathSize);
+//                    for(int m = 0; m<pathSize;m++){
+//                        printf("%s/n",path[m]);
+//                    }
                 }
             }
             else{
